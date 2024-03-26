ec8980669f65ca4f5dae14b827fc9cd77c4e5714
Zahira Khan <66169164+zahirakhan@users.noreply.github.com>
2024-03-26 08:59:09 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const fruits: string[] = [
+  "apple",
+  "banana",
+  "orange",
+  "grape",
+  "strawberry",
+  "pineapple"
+];
+
+const randomFruitIndex = Math.floor(Math.random() * fruits.length);
+const randomFruit = fruits[randomFruitIndex];
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessFruit",
+    type: "input",
+    message: chalk.blue(
+      "Please Guess a Fruit (apple, banana, orange, grape, strawberry, pineapple): "
+    ),
+  },
+]);
+
+const userGuess = answer.userGuessFruit; 
+if (userGuess === randomFruit) {
+  console.log(chalk.greenBright("CONGRATULATIONS! You Guessed the Right Fruit"));
+} else {
+  console.log(chalk.bgRed(`SORRY! You Guessed Wrong. The correct fruit was: ${randomFruit}`));
+}
